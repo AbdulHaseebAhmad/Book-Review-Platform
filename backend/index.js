832c4a144adf7af0e658db981e8d746f793d8eca
AbdulHaseebAhmad <79489830+AbdulHaseebAhmad@users.noreply.github.com>
2024-07-29 13:25:07 +0500

    Changed ACess Control Allor Origin in Options

--- backend/index.js
+++ backend/index.js
@@ -48,13 +48,12 @@ app.use(
   })
 );
 
-app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', 'https://book-review-platform-mu.vercel.app/'); // Replace with your frontend domain
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.sendStatus(204); // No content
-});
+app.options('*',  cors({
+  origin: ["http://localhost:5173","https://book-review-platform-mu.vercel.app/"], 
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization", 'X-Requested-With'],
+  credentials: true,
+}));
 
 
 app.use(userRoute);

966ec9fd9c8423cb82e2b580a43cc5fbbc2664b4
AbdulHaseebAhmad <79489830+AbdulHaseebAhmad@users.noreply.github.com>
2024-07-29 14:16:17 +0500

    Ready For Local Host

--- backend/index.js
+++ backend/index.js
@@ -41,7 +41,7 @@ mongoose
 
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://book-review-platform-mu.vercel.app/"], 
+    origin: ["http://localhost:5173"], 
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
